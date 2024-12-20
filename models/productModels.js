ddde3305e3b995c9bf02faf011f2655284c524b2
John-Landry <johnlandry33@gmail.com>
2024-12-20 13:32:12 -0500

    x

---
+++ models/productModels.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Enter a name"],
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+},
+    {
+        timestamps: true
+    }
+)
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
+
--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express();
+const Product = require('./models/productModels')
+const app = express()
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello john');
 })
 
-app.get('/johnpage', (req, res) => {
+app.get('/john', (req, res) => {
     res.send('john---------- page');
 })
 
+app.post('/product', async (req, res) => {
+    try {
+        const Product = await Product.create(req.body);
+        res.status(200).json(Product);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
 
 mongoose.connect(
     'mongodb+srv://johnclandry:hDqIrxpChOdvMDKc@cluster0.emu6y.mongodb.net/?retryWrites=true&w=majority'
 )
     .then(() => {
         console.log('Connected to MongoDB');
+        app.listen(8000, () => {
+            console.log('Server is running on port 3000');
+        });
     })
     .catch((error) => {
         console.log('Error connecting to MongoDB:', error);
